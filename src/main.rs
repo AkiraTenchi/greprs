f0025f5f30b4ba56f5221a596c096e180cbb28a3
AkiraTenchi <jimjan2002@gmail.com>
2023-12-16 16:27:11 +0100

    improved error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 use colored::Colorize;
 
@@ -7,10 +8,11 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
 
-    let content = std::fs::read_to_string(&args.path).expect("could not read the file");
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("Could not read file `{}`", args.path.display()))?;
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
@@ -24,4 +26,5 @@ fn main() {
             println!();
         }
     }
+    Ok(())
 }
