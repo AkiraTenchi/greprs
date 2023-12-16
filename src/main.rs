d3f19d9befe943838ffa66940786e76620d8ca64
AkiraTenchi <jimjan2002@gmail.com>
2023-12-16 15:59:30 +0100

    added basic args functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("{:?}  {:?}", args.pattern, args.path);
 }
